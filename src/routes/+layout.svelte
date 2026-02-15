2151df7cd7391fa3e38893bb89e26ca8466fa9a4
Dilukshan <65407969+dilukshann7@users.noreply.github.com>
2026-02-16 00:55:36 +0530

    Feat: Add theme toggling and toast notifications to global layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import "./layout.css";
+	import favicon from "$lib/assets/favicon.svg";
+	import { ModeWatcher } from "mode-watcher";
+	import { Toaster } from "svelte-sonner";
 
 	let { children } = $props();
 </script>
 
+<ModeWatcher />
+<Toaster richColors position="top-right" />
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 {@render children()}
