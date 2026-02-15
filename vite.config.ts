3a88e9e59ade619e7a37826963db26129dfa06d5
Dilukshan <65407969+dilukshann7@users.noreply.github.com>
2026-02-16 01:04:09 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
 	build: {
 		rollupOptions: {
-			external: ['valibot', '@valibot/to-json-schema']
+			external: []
 		}
 	}
 });
