816bd9ece10b524938399ec11e4247ecaca77e1f
Dilukshan <65407969+dilukshann7@users.noreply.github.com>
2026-02-17 15:24:35 +0530

    feat: Integrate Better-Auth with SvelteKit

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,15 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { sveltekitCookies } from "better-auth/svelte-kit";
+import { getRequestEvent } from "$app/server";
 import { db } from "./server/db";
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "pg",
     }),
-      emailAndPassword: { 
-    enabled: true, 
-  }, 
+    plugins: [sveltekitCookies(getRequestEvent)],
+    emailAndPassword: {
+        enabled: true,
+    },
 });
